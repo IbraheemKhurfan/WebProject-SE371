13ce1508f45caefd81b3d51c232735683a416728
fouad20000 <fouad.alkadri@hotmail.com>
2022-05-19 00:31:22 +0300

    Update var.php

--- var.php
+++ var.php
@@ -11,11 +11,11 @@ $titel="CDMA2022";
 # Fouad Section Login, SignUp
 
 #SignUp , Login
-$name1 = $_POST['name'];
-$phone = $_POST['phone'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$repassword = $_POST['repassword'];
+$name1 = $_POST["name"];
+$phone = $_POST["phone"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$repassword = $_POST["repassword"];
 
 
 
