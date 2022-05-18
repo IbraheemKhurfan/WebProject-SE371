517b1e97525a0e652ef89f34d294df8e1035fb8b
IbraheemKhurfan <ibraheemkhurfan@gmail.com>
2022-05-19 01:25:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -107,13 +107,13 @@ include("var.php");
                         <!-- Password input -->
                         <div class="form-outline mb-3">
                             <label class="form-label" for="password">Password</label>
-                            <input type="password" id="password" class="form-control form-control-lg" minlength="4" maxlength="42" placeholder="Enter password" required/>
+                            <input type="password" id="password" name="password" class="form-control form-control-lg" minlength="4" maxlength="42" placeholder="Enter password" required/>
                         </div>
                         
                         <!-- Repeat Password input--> 
                         <div class="form-outline mb-3">
-                            <label class="form-label" for="password">Confirm Password</label>
-                            <input type="password" id="repassword" class="form-control form-control-lg" minlength="4" maxlength="42" placeholder="Confirm your password" required/>
+                            <label class="form-label" for="repassword">Confirm Password</label>
+                            <input type="password" id="repassword"name="repassword" class="form-control form-control-lg" minlength="4" maxlength="42" placeholder="Confirm your password" required/>
                         </div>
                         
                       <!--  <label>
