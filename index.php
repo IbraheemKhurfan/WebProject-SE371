ad7b4259e5cb22b79c2333b1a58e129b3cfe12cb
moe-skaik <218110540@psu.edu.sa>
2022-05-24 21:02:08 +0300

    skaik

--- index.php
+++ index.php
@@ -1383,7 +1383,7 @@ gallery[i].onclick = () =>{
   <div class="container">
         <div class="col-md-12 text-center">
             <button type="button" class="btn btn-primary"> <a href="LogIn.php"
-                class="text-white">Submit</a></button>
+                class="text-white">Register</a></button>
             
         </div>      
 </div>
